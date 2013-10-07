15510d58254b4a5b3535af1b0781a0b55ab22b8a
Nyitrai Jozsef <nyitrai.jozsef@gmail.com>
2013-10-07 18:53:04 +0200

    handling redirection, nicer output

--- status.js
+++ status.js
@@ -1,23 +1,28 @@
 var system = require('system');
 if (system.args.length === 1) {
-  console.log('Try to pass some args when invoking this script!');
+  console.log('Try to pass an URL as argument when invoking this script!');
   phantom.exit();
 }
 
 var url = system.args[1];
-
 var page = require('webpage').create();
-
+var redirect_url = url;
 page.onResourceReceived = function(response) {
-  var message = '';
-  if (response.url == url || response.url == url + '/' && response.stage == 'start') {
-    if (response.status >= 200 && response.status < 400) {
-      message += 'SUCCESS('      
+  var message = ''
+  if (response.redirectURL) {
+    redirect_url = response.redirectURL;
+  }
+  if ([url, url + '/', redirect_url].indexOf(response.url) >= 0 && response.stage == 'start') {
+    if (response.status >= 200 && response.status < 300) {
+      message += 'SUCCESS  (';
+    } else if (response.status >= 300 && response.status < 400) {
+      message += 'REDIRECT (';
     } else {
-      message += 'ERROR(';
+      message += 'ERROR    (';
     }
       
     message += String(response.status) + ')    ' + response.url;
+
     console.log(message);
   }
 };
